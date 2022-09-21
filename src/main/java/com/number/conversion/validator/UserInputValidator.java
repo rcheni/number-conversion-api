c0be128fcabbed7afd43b5664c69bad825d4174e
Ramu Cheni <ramu.cheni@astrazeneca.com>
2022-09-21 20:08:39 +0100

    Validation logic added for Integer number user input

--- src/main/java/com/number/conversion/validator/UserInputValidator.java
+++ src/main/java/com/number/conversion/validator/UserInputValidator.java
@@ -1,6 +1,11 @@
 package com.number.conversion.validator;
 
 public class UserInputValidator {
-    // TODO: Method 1 : Business logic to validate Integer number user input
+    private static final String INTEGER_NUMBER_PATTERN = "[0-9]+";
+
+    public Boolean isUserInputValidInteger(String userInput) {
+        return userInput.matches(INTEGER_NUMBER_PATTERN);
+    }
+
     // TODO: Method 2 : Business logic to validate Roman number user input
 }
--- src/test/java/com/number/conversion/validator/UserInputValidatorTest.java
+++ src/test/java/com/number/conversion/validator/UserInputValidatorTest.java
@@ -1,6 +1,47 @@
 package com.number.conversion.validator;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class UserInputValidatorTest {
-    // TODO: Test1 : Test UserInputValidator.Method1 which validates Integer number user input
+    private UserInputValidator userInputValidator;
+
+    @BeforeEach
+    public void setup() {
+        this.userInputValidator = new UserInputValidator();
+    }
+
+    @Test
+    public void shouldReturnTrueIfTheGivenIntegerNumberIsValid() {
+        // Given
+        String userInput = "1002";
+
+        // When
+        Boolean userInputValidInteger = userInputValidator.isUserInputValidInteger(userInput);
+
+        // Then
+        assertTrue(userInputValidInteger);
+    }
+
+    @Test
+    public void shouldReturnFalseIfTheGivenIntegerIsNotValid() {
+        // Given
+        String userInput = "1002a";
+
+        // When
+        Boolean userInputNotValidInteger = userInputValidator.isUserInputValidInteger(userInput);
+
+        // Then
+        assertFalse(userInputNotValidInteger);
+    }
     // TODO: Test2 : Test UserInputValidator.Method2 which validates Roman number user input
+
+    @AfterEach
+    public void tearDown() {
+        userInputValidator = null;
+    }
 }
