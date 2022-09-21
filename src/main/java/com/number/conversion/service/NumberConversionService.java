0291ee7476d87f5eed0d09d54d5033622bd9c907
Ramu Cheni <ramu.cheni@astrazeneca.com>
2022-09-21 19:01:57 +0100

    Added business logic to convert Integer number to Roman number

--- src/main/java/com/number/conversion/service/NumberConversionService.java
+++ src/main/java/com/number/conversion/service/NumberConversionService.java
@@ -1,6 +1,30 @@
 package com.number.conversion.service;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class NumberConversionService {
-    // TODO: Method 1: Business logic to convert Integer number to Roman number.
+    public static final List<Integer> intNumbers = List.of(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1);
+    public static final List<String> romanLetters = List.of("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I");
+
+    public String convertIntegerToRomanNumber(Integer intValue) {
+        StringBuilder romanNumber = new StringBuilder();
+
+        AtomicInteger atomicInteger = new AtomicInteger(intValue);
+
+        Integer idx = 0;
+
+        for (Integer intNumber : intNumbers) {
+            while (intValue >= intNumber) {
+                intValue -= intNumber;
+                romanNumber.append(romanLetters.get(idx));
+            }
+
+            idx++;
+        }
+
+        return romanNumber.toString();
+    }
+
     // TODO: Method 2: Business logic to convert Roman number to Integer number.
 }
--- src/test/java/com/number/conversion/service/NumberConversionServiceTest.java
+++ src/test/java/com/number/conversion/service/NumberConversionServiceTest.java
@@ -1,9 +1,42 @@
 package com.number.conversion.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class NumberConversionServiceTest {
+
+    private NumberConversionService numberConversionService;
+
+    @BeforeEach
+    public void setUp() {
+        numberConversionService = new NumberConversionService();
+    }
+
+
     // TODO: Test1 : Test UserInputValidator.Method1 which validates Integer number user input
     // TODO: Test2 : Test UserInputValidator.Method2 which validates Roman number user input
 
-    // TODO: Test1 : Test NumberConversionService.Method1 which converts Integer number to Roman Number
-    // TODO: Test2 : Test NumberConversionService.Method1 which converts Roman number to Integer numnber
+    @Test
+    public void shouldReturnRomanNumberForTheGivenValidInteger() {
+        // Given
+        Integer intValue = 1002;
+
+        String expectedRomanNumber = "MII";
+
+        // When
+        String romanNumber = numberConversionService.convertIntegerToRomanNumber(intValue);
+
+        // Then
+        assertEquals(expectedRomanNumber, romanNumber);
+    }
+
+    // TODO: Test4 : Test NumberConversionService.Method1 which converts Roman number to Integer number
+
+    @AfterEach
+    public void tearDown() {
+        numberConversionService = null;
+    }
 }
